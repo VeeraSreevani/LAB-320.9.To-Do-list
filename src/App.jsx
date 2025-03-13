02474600e1104147cf207b324030a91ed4a489e7
VeeraSreevani <sreevani.solige@gmail.com>
2025-03-13 01:31:02 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Todo from "./components/Todo";
 import initialState from "./data/data";
-import { useReducer,useState } from "react";
+import { useReducer, useState } from "react";
 
 // Reducer
 function todosReducer(state, action) {
@@ -14,10 +14,24 @@ function todosReducer(state, action) {
         id: state.length + 1,
         userId: 1,
       };
-      //case "toggle_todo"
-      //return(state.map(todo=> todo.id ===action.payload ?{...todo, completed: !todo.completed} :todo)
       return [newTodo, ...state];
     }
+    case "toggle_todo":{
+      return(state.map(todo=> todo.id ===action.payload ?{...todo, completed: !todo.completed} :todo))}
+
+      case "delete_todo":{
+        return state.filter(todo => todo.id !== action.payload)
+      }
+
+      case "edit_todo":{
+        return state.map(todo=>
+          todo.id === action.payload.id ? 
+          {...todo, title: action.payload.title}
+          :todo
+        );
+      }
+
+
     default: {
       return state;
     }
@@ -25,11 +39,13 @@ function todosReducer(state, action) {
 }
 
 function App() {
+
   const [newTodo, setNewTodo] = useState("");
   const [todos, dispatch] = useReducer(todosReducer, initialState);
   // console.log(todos);
   const handleClick = () => {
     dispatch({ type: "add_todo", payload: newTodo });
+    setNewTodo("");
   };
   return (
     <>
@@ -43,7 +59,7 @@ function App() {
       <button onClick={handleClick}>Add</button>
 
       {todos.map((t) => (
-        <Todo todo={t} key={t.id} />
+        <Todo todo={t} key={t.id} dispatch= {dispatch} />
       ))}
     </>
   );
@@ -51,4 +67,4 @@ function App() {
 
 export default App;
 
-// {...t}
+
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,18 +1,66 @@
+
 import { useState } from "react";
-function Todo({ todo }) {
+
+function Todo({ todo,dispatch }) {
   console.log(todo);
+
   const { title, completed, id } = todo;
-  const [isChecked, setIsChecked] = useState(false);
+  const [isChecked, setIsChecked] = useState(completed);
+  const [editedTitle,setEditedTitle] = useState(title);
+  const[isEditing, setIsEditing] = useState(false)
+  //toggling the completion status
+  const handleCheckboxChange = (e)=>{
+    setIsChecked(e.target.value);
+    dispatch({
+      type: "toogle_todo",
+      payload: id,
+    });
+  };
+
+  //handling edited todo
+  const handleEditChange= ()=>{
+    dispatch({
+      type: "edit_todo",
+      payload: {id,title:editedTitle}
+    })
+    setIsEditing(false);
+  };
+
+
   return (
     <div style={styles}>
       <input
         type="checkbox"
         checked={isChecked}
-        onChange={(e) => setIsChecked(e.target.checked)}
+        onChange={handleCheckboxChange}
       />
-      <h2>{title}</h2>
-      <button>Edit</button>
-      <button>Delete</button>
+      {
+        isEditing ? (
+          <input
+            type = "text"
+            value = {editedTitle}
+            onChange = {(e)=>setEditedTitle(e.target.value)}
+            />
+        ):(
+          <h2 style={{textDecoration: isChecked ? "line-through":"none"}}> {title}</h2>
+        )
+      }
+     
+      <button 
+        onClick={()=>setIsEditing(true)}>
+        Edit
+      </button>
+
+      <button
+        disabled={!isChecked}
+        onClick={()=>dispatch({type: "delete_todo", payload: id})}>
+        Delete
+      </button>
+      {
+        isEditing && (
+          <button onClick={handleEditChange}>Save</button>
+        )
+      }
     </div>
   );
 }
